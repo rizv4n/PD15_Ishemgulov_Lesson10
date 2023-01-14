35ef5a5b97a65ef859d7a7152a3b08be2c7f3341
rizv4n <rizvanishemgulov@gmail.com>
2023-01-14 22:02:24 +0500

    PD15_Ishemgulov_Lesson14

--- sky-lessons/Lesson14/app/find_movie.py
+++ sky-lessons/Lesson14/app/find_movie.py
@@ -4,18 +4,19 @@ import json
 direct = '../netflix.db'
 
 
-def get_results(element_type, year):
+def get_results(element_type, year, genre):
     with sqlite3.connect(direct) as con:
         cur = con.cursor()
         sqlite_query = (f"""
-            SELECT title, description
+            SELECT title, description, listed_in
             FROM 'netflix'
             WHERE type == '{element_type}'
             AND release_year == {year}
+            AND listed_in LIKE '%{genre}%'
         """)
         cur.execute(sqlite_query)
         executed_query = cur.fetchall()
     return json.dumps(executed_query)
 
 
-print(get_results('TV Show', 2021))
+print(get_results('TV Show', 2021, 'dramas'))
--- sky-lessons/Lesson14/main.py
+++ sky-lessons/Lesson14/main.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from app.seacrh import get_title_by_word, correct_result_title
 from app.year_to_year import get_title_by_year, correct_result_year
 from app.rating import get_title_by_rating, correct_result_rating
+from app.genre import get_title_by_genre, correct_result_genre
 
 app = Flask(__name__)
 
 direct = "netflix.db"
+
+
+@app.route('/')
+def main_page():
+    return render_template('main_page.html')
 
 
 @app.route('/movie/<title>')
@@ -27,7 +33,7 @@ def search_by_rating(rating):
         rating_list = ('G', '')
         movies = get_title_by_rating(rating_list, direct)
         return correct_result_rating(movies)
-    elif rating == 'family ':
+    elif rating == 'family':
         rating_list = ('G', 'PG', 'PG-13')
         movies = get_title_by_rating(rating_list, direct)
         return correct_result_rating(movies)
@@ -37,6 +43,12 @@ def search_by_rating(rating):
         return correct_result_rating(movies)
     else:
         return 'Некорректный выбор рейтинга'
+
+
+@app.route('/genre/<genre>')
+def search_by_genre(genre):
+    movies = get_title_by_genre(genre, direct)
+    return correct_result_genre(movies)
 
 
 if __name__ == '__main__':
